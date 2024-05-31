d70e5d76d338644eb3377e2f626199a1681e7a41
SSeries <63185402+S-Series@users.noreply.github.com>
2024-05-31 17:06:24 +0900

    2024.05.31 || 17:06

--- Assets/Scripts/MusicInfoDisplay.cs
+++ Assets/Scripts/MusicInfoDisplay.cs
@@ -1,18 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class MusicInfoDisplay : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] SpriteRenderer MusicJacket;
+    [SerializeField] SpriteRenderer MusicFrame;
+    [SerializeField] SpriteRenderer PlayRank;
+    [SerializeField] SpriteRenderer PlayMedal;
 
-    // Update is called once per frame
-    void Update()
+    [SerializeField] TextMeshPro MusicTitle;
+    [SerializeField] TextMeshPro MusicArtist;
+    [SerializeField] TextMeshPro MusicBPM;
+
+    [SerializeField] SpriteRenderer[] HighScore; //# [0][1][2][3][4][5][6][7]
+    [SerializeField] DiffHolder[] Difficultys;
+    [SerializeField] SpriteRenderer[] DiffIndexBoxes;
+
+    public void UpdateMusicInfo(MusicData data)
     {
-        
+        int index, lineIndex, difficulty;
+        index = MusicSelectSystem.s_DiffIndex;
+        lineIndex = (int)MusicSelectSystem.s_LineMode;
+
+        for (int i = 0, j = index; i < 5; i++, j++)
+        {
+            Difficultys[i].ApplyDiff(data.musicGameDatas[lineIndex].diff[i], i);
+            if (i == index)
+            {
+                
+            }
+        }
     }
 }
